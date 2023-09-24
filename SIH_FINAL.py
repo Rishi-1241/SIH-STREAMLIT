b4a90cafb4baaba53bbc692f738dad0ad24c5a7d
Prakhar Agrawal <123866908+Rishi-1241@users.noreply.github.com>
2023-09-25 01:26:45 +0530

    SIH_FINAL.py

--- SIH_FINAL.py
+++ SIH_FINAL.py
@@ -12,16 +12,16 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Load data for Ayurvedic medicine, hospitals, and colleges
-medicine_data = pd.read_csv(r"C:\Users\Prakhar Agrawal\Downloads\Final_data.csv")
+medicine_data = pd.read_csv(r"C:/Users/Prakhar Agrawal/Downloads/Final_data.csv")
 medicine_data = medicine_data.dropna()
 
-hospital_data = pd.read_csv(r"C:\Users\Prakhar Agrawa\Downloads\hospitals.csv")
+hospital_data = pd.read_csv(r"C:/Users/Prakhar Agrawal/Downloads/hospitals.csv")
 hospital_data = hospital_data.dropna()
 
-college_data = pd.read_csv(r"C:\Users\Prakhar Agrawal\Downloads\ayurvedic-colleges_2- (1).csv")
+college_data = pd.read_csv(r"C:/Users/Prakhar Agrawal/Downloads/ayurvedic-colleges_2- (1).csv")
 college_data = college_data.drop_duplicates(subset='College ID')
 
-# Function to recommend Ayurvedic Medicines
+
 def recommend_medicines(user_disease):
     tfidf_vectorizer = TfidfVectorizer()
     tfidf_matrix = tfidf_vectorizer.fit_transform(medicine_data['Diseases Cured'])
